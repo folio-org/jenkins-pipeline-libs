ecb5e49bbcc73dac47b636432759d2cdbb3c3a6b
John Malconian <malc@indexdata.com>
2017-08-24 16:06:10 +0000

    use BUILD_DISPLAY_NAME

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -12,7 +12,7 @@ def call(String buildStatus = 'STARTED') {
   // Default values
   def color = 'RED'
   def colorCode = '#FF0000'
-  def subject = "${buildStatus}: '${env.JOB_NAME} #${env.BUILD_NUMBER}'"
+  def subject = "${buildStatus}: '${env.JOB_NAME} ${env.BUILD_DISPLAY_NAME}'"
   def summary = "${subject} (<${env.BUILD_URL}|Open>)"
 
   // Override default values based on build status
