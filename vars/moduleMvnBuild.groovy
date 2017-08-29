7ff8a5ab700d0bfd418164e7a0d3bb98bc097156
John Malconian <malc@indexdata.com>
2017-08-29 13:55:09 +0000

    moduleMvnBuild.groovy

---
+++ vars/moduleMvnBuild.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/groovy
+
+
+def call() {
+
+  pipeline {
+    agent {
+      node {
+        label 'folio-jenkins-slave-docker'
+      }
+    }
+
+    stages {
+      stage('Prep') {
+        steps {
+          script {
+            currentBuild.displayName = "#${env.BUILD_NUMBER}-${env.JOB_BASE_NAME}"
+        }
+
+      stage('Mvn Build') {
+        steps {
+          withMaven(jdk: 'OpenJDK 8 on Ubuntu Docker Slave Node',
+                    maven: 'Maven on Ubuntu Docker Slave Node',
+                    options: [junitPublisher(disabled: false,
+                    ignoreAttachments: false),
+                    artifactsPublisher(disabled: false)]) {
+
+            sh 'mvn -DskipTests integration-test'
+          }
+        }
+      }
+
+    } // end stages
+
+  } // end pipeline
+ 
+} 
+
+  
