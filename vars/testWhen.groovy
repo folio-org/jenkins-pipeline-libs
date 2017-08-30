eb48c94e7b1dc3758fc391d81900b94331bea3fc
John Malconian <malc@indexdata.com>
2017-08-30 14:27:10 +0000

    x

--- vars/testWhen.groovy
+++ vars/testWhen.groovy
@@ -24,7 +24,7 @@ def call(body) {
 
       stage('test') {
          when {
-           expression { config.buildDocker ==~ /(?i)(Y|YES|T|TRUE)/ }
+           expression { config.buildDocker ==~ '(yes|true)' }
          }
         steps {
            echo "It's true!"
