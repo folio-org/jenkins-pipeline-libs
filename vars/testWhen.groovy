753520120c6ff442c8e9de6b41683cc2b2dd0166
John Malconian <malc@indexdata.com>
2017-08-31 15:15:48 +0000

    scripted test case

--- vars/testWhen.groovy
+++ vars/testWhen.groovy
@@ -7,23 +7,52 @@ def call(body) {
   body.delegate = config
   body()
 
-  pipeline {
+  node {
 
-    agent any 
-
-    stages {
-      stage('test') {
-        when {
-          branch 'master'
-        }
-        steps {
-          echo "Message: $config.message"  
-          echo "Branch is: $env.BRANCH_NAME"
-        }
+    try {
+      stage('One') {
+        echo "Branch is: $env.BRANCH_NAME"
+        echo "Perform Stage One"
       }
-    }
+
+      stage('Two') {
+        echo "Perform Stage Two"
+      }
+   
+      if ( env.BRANCH_NAME == 'master' ) {    
+        echo "config.doDocker is: $config.doDocker" 
+
+        if ( config.doDocker ==~ /(?i)(Y|YES|T|TRUE)/ ) {
+          stage('Docker') {
+            echo "Building Docker" 
+          }
+          stage('Docker Publish') {
+            echo "Publishing Docker"
+          }
+        } 
+        if (config.publishModDescriptor ==~ /(?i)(Y|YES|T|TRUE)/) {
+          stage('Publish Module Descriptor') {
+            echo "Publishing Module Descriptor"
+          }
+        }
+        if (config.publishAPI ==~ /(?i)(Y|YES|T|TRUE)/) {
+          stage('Publish API Docs') {
+          echo "Publishing API docs"
+          }
+        }
+      } 
+    } // end try
+    catch (err) {
+      currentBuild.result = 'FAILED'
+      echo "Build Result: $currentBuild.result"
+      throw err
     
-  } // end pipeline
- 
+    }
+    finally {
+      echo "Send some notifications"
+      echo "Do some cleanup"
+    }
+  } //end node
+    
 } 
 
