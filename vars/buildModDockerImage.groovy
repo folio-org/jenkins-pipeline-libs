effd33b61357a92a1eafda7ea96c792b583460ea
John Malconian <malc@indexdata.com>
2017-08-31 16:42:19 +0000

    x

--- vars/buildModDockerImage.groovy
+++ vars/buildModDockerImage.groovy
@@ -5,8 +5,11 @@
  * Send notifications based on build status string
  */
 
-def call(String name, String version) {
-  
+def call(String name, String version, String fatJar) {
+
+  def dockerFile = libraryResource 'org.folio.Dockerfile.JavaModule'
+  def dockerEntrypoint = libraryResource 'org.folio.dockerentrypoint.JavaModule'
+
   sh """
   cat > .dockerignore << EOF
 *
@@ -15,6 +18,10 @@ def call(String name, String version) {
 !target/*.jar
 EOF
   """
-  sh "docker build -t ${name}:$version} ."
+
+  sh "echo $dockerFile > ${env.WORKSPACE}/Dockerfile"
+  sh "echo $dockerEntrypoint > ${env.WORKSPACE}/dockerentrypoint.sh"
+  
+  sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' ."
   sh "docker tag ${name}:${version} ${name}:latest"  
 }
