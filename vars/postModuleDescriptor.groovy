32c635128e316c6ba6cde0179ab098c9b859a2fb
John Malconian <malc@indexdata.com>
2017-09-08 15:44:58 +0000

    fix regexp

--- vars/postModuleDescriptor.groovy
+++ vars/postModuleDescriptor.groovy
@@ -11,14 +11,17 @@ def call(String modDescriptor,String version) {
   def folioRegistry = 'http://folio-registry-test.aws.indexdata.com:9130/_/proxy/modules'
 
   // Add build number to version if snapshot
-  if (version ==~ /.*-SNAPSHOT$/) {
+  if (version ==~ /.*-SNAPSHOT.*/) { 
     sh "mv $modDescriptor ${modDescriptor}.tmp"
     sh """
-      jq '.id |= \"${version}.${env.BUILD_NUMBER}\" ${modDescriptor}.tmp > $modDescriptor
+      jq '.id |= \"${version}\" ${modDescriptor}.tmp > $modDescriptor
     """
   }
 
   def request = readFile("$modDescriptor")
+  echo "Module Descriptor:"
+  echo "$request"
+
   httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', 
               requestBody: "$request", url: "$folioRegistry"
       
