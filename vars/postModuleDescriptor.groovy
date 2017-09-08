a7b10a999fba4b71b2013f853d68e10e5ae68c80
John Malconian <malc@indexdata.com>
2017-09-08 14:04:53 +0000

    post ModDescriptor function

---
+++ vars/postModuleDescriptor.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+
+/*
+ * Send notifications based on build status string
+ */
+
+def call(String modDescriptor,String version) {
+ 
+  //def folioRegistry = 'http://folio-registry.aws.indexdata.com:9130/_/proxy/modules'
+  def folioRegistry = 'http://folio-registry-test.aws.indexdata.com:9130/_/proxy/modules'
+
+  // Add build number to version if snapshot
+  if (version ==~ /.*-SNAPSHOT$/) {
+    sh "mv $modDescriptor ${modDescriptor}.tmp"
+    sh """
+      jq '.id |= \"${version}\" ${modDescriptor}.tmp > $modDescriptor
+    """
+  }
+
+  def request = readFile("$modDescriptor")
+  httpRequest acceptType: 'APPLICATION_JSON', contentType: 'APPLICATION_JSON', httpMode: 'POST', 
+              requestBody: "$request", url: "$folioRegistry"
+      
+}
