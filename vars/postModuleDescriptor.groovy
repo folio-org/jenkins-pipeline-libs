b871a122cc21bccc46b415ee390ef381c4df30e7
John Malconian <malc@indexdata.com>
2017-09-09 23:57:04 +0000

    update registry to folio-registry

--- vars/postModuleDescriptor.groovy
+++ vars/postModuleDescriptor.groovy
@@ -7,8 +7,7 @@
 
 def call(String modDescriptor,String name,String version) {
  
-  //def folioRegistry = 'http://folio-registry.aws.indexdata.com:9130/_/proxy/modules'
-  def folioRegistry = 'http://folio-registry-test.aws.indexdata.com:9130/_/proxy/modules'
+  def folioRegistry = 'http://folio-registry.aws.indexdata.com:9130/_/proxy/modules'
 
   // Add build number to version if snapshot
   if (version ==~ /.*-SNAPSHOT.*/) { 
