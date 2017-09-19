4af69827435d340d7cb40d74bc4643504eb968f3
John Malconian <malc@indexdata.com>
2017-09-19 12:48:41 +0000

    initiate sonarqube scanfor mvn PRs

--- vars/buildMvnMod.groovy
+++ vars/buildMvnMod.groovy
@@ -56,7 +56,6 @@ def call(body) {
         }
       }
 
-
       if (( env.BRANCH_NAME == 'master' ) ||     
          ( env.BRANCH_NAME == 'jenkins-test' )) {
 
@@ -115,6 +114,25 @@ def call(body) {
           }
         }
       } 
+      else {
+        if ($env.CHANGE_ID) {
+          echo "PR request: $env.CHANGE_ID"
+          stage('SonarQube Scan') {
+            withCredentials([[$class: 'StringBinding', 
+                              credentialsId: 'folio-jenkins-github-token', 
+                              variable: 'GITHUB_ACCESS_TOKEN']]) {
+              withSonarQubeEnv('SonarCloud') {
+                sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " +
+                       "-Dsonar.organization=folio-org -Dsonar.verbose=true " +
+                       "-Dsonar.analysis.mode=preview " +
+                       "-Dsonar.github.pullRequest=${env.CHANGE_ID} " +
+                       "-Dsonar.github.oauth=${GITHUB_ACCESS_TOKEN}"
+              }
+            }
+          }
+        }
+      } 
+          
     } // end try
     catch (Exception err) {
       currentBuild.result = 'FAILED'
