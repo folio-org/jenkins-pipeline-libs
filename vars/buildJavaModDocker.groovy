2b8b4b3e963dd8c84e7d7413b8524305f17ad207
John Malconian <malc@indexdata.com>
2017-10-13 15:24:11 +0000

    test okapi-433

--- vars/buildJavaModDocker.groovy
+++ vars/buildJavaModDocker.groovy
@@ -5,7 +5,7 @@
  * Send notifications based on build status string
  */
 
-def call(String name, String version) {
+def call(String name, String version, List dockerDir) {
 
   def fatJar = "${name}-fat.jar"
   def dockerFile
@@ -13,11 +13,14 @@ def call(String name, String version) {
   def buildArg
 
   echo "Fat Jar is: $fatJar"
- 
-  if (fileExists('.dockerignore')) {
+  
+  for (d in dockerDir) {
+    dir("${env.WORKSPACE}/${d}") 
+
+    if (fileExists('.dockerignore')) {
       echo "Using existing .dockerignore"
-  }
-  else {
+    }
+    else {
       echo "Creating .dockerignore"
       sh """
       cat > .dockerignore << EOF
@@ -27,36 +30,35 @@ def call(String name, String version) {
 !target/*.jar
 EOF
       """
-  }
+    }
 
-  if (fileExists('Dockerfile')) {
-    echo "Found existing Dockerfile." 
-    buildArg = 'no'
-  }
-  else {
-    echo "Dockerfile not found.  Using Java template."
-    dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
-    writeFile file: 'Dockerfile', text: "$dockerFile"
-    buildArg = 'yes'
-    
-  }
+    if (fileExists('Dockerfile')) {
+      echo "Found existing Dockerfile." 
+      buildArg = 'no'
+    }
+    else {
+      echo "Dockerfile not found.  Using Java template."
+      dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
+      writeFile file: 'Dockerfile', text: "$dockerFile"
+      buildArg = 'yes'
+    }
 
-  if (fileExists('docker/docker-entrypoint.sh')) {
-    echo "Found existing docker-entrypoint.sh script." 
-  }
-  else {
-   echo "docker-entrypoint.sh not found. Using Java entrypoint template."
-   dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
-   writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
-  }
+    if (fileExists('docker/docker-entrypoint.sh')) {
+      echo "Found existing docker-entrypoint.sh script." 
+    }
+    else {
+      echo "docker-entrypoint.sh not found. Using Java entrypoint template."
+      dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
+      writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
+    }
 
-  if (buildArg == 'yes') {
-    sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
-  }
-  else {
-    sh "docker build --tag ${name}:${version} ."
-  }
+    if (buildArg == 'yes') {
+      sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
+    }
+    else {
+      sh "docker build --tag ${name}:${version} ."
+    }
   
-  sh "docker tag ${name}:${version} ${name}:latest"  
-  
+    sh "docker tag ${name}:${version} ${name}:latest"  
+  } 
 }
---
+++ vars/buildMvnModTest.groovy
@@ -0,0 +1,98 @@
+#!/usr/bin/groovy
+
+
+def call(body) {
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  node('folio-jenkins-slave-docker') {
+
+    try {
+      stage('Checkout') {
+        deleteDir()
+        currentBuild.displayName = "#${env.BUILD_NUMBER}-${env.JOB_BASE_NAME}"
+        sendNotifications 'STARTED'
+
+         checkout([
+                 $class: 'GitSCM',
+                 branches: scm.branches,
+                 extensions: scm.extensions + [[$class: 'SubmoduleOption',
+                                                       disableSubmodules: false,
+                                                       parentCredentials: false,
+                                                       recursiveSubmodules: true,
+                                                       reference: '',
+                                                       trackingSubmodules: false]],
+                 userRemoteConfigs: scm.userRemoteConfigs
+         ])
+
+         echo "Checked out $env.BRANCH_NAME"
+
+        // project name is different from mod name specified in package.json
+        def proj_name = sh(returnStdout: true, script: 'git config remote.origin.url | awk -F \'/\' \'{print $5}\' | sed -e \'s/\\.git//\'').trim()
+        env.project_name = proj_name
+        echo "$env.project_name"
+
+      }
+
+      stage('Maven Build') {
+        def mvn_artifact = readMavenPom().getArtifactId() 
+        def mvn_version =  readMavenPom().getVersion()
+        env.name = "$mvn_artifact"
+
+        if (mvn_version ==~ /.*-SNAPSHOT$/) {
+          echo "This is a snapshot"
+          env.version = "${mvn_version}.${env.BUILD_NUMBER}"
+        }
+        else {
+          env.version = "$mvn_version"
+        }
+
+        echo "Building Maven artifact: ${env.name} Version: ${env.version}"
+            
+        withMaven(jdk: 'OpenJDK 8 on Ubuntu Docker Slave Node',
+                    maven: 'Maven on Ubuntu Docker Slave Node',
+                    options: [junitPublisher(disabled: false,
+                    ignoreAttachments: false),
+                    artifactsPublisher(disabled: false)]) {
+
+          sh 'mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install'
+
+        }
+      }
+
+      if ( config.doDocker ==~ /(?i)(Y|YES|T|TRUE)/ ) {
+        stage('Docker Build') {
+          def dockerDir
+          if (config.subDirs) {
+            dockerDir = config.subDir
+          } 
+          else 
+            dockerDir = env.WORKSPACE
+          }
+          echo "Building Docker image $env.name:$env.version" 
+          buildJavaModDocker(env.name,env.version,dockerDir) 
+      }
+          
+    } // end try
+    catch (Exception err) {
+      currentBuild.result = 'FAILED'
+      println(err.getMessage());
+      echo "Build Result: $currentBuild.result"
+      throw err
+    }
+    finally {
+      echo "Clean up any temporary docker artifacts"
+      sh "docker rmi ${env.name}:${env.version} || exit 0"
+      sh "docker rmi ${env.name}:latest || exit 0"
+      sh "docker rmi ${env.repository}/${env.name}:${env.version} || exit 0"
+      sh "docker rmi ${env.repository}/${env.name}:latest || exit 0"
+
+      sendNotifications currentBuild.result
+    }
+
+  } //end node
+    
+} 
+
