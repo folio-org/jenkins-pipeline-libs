249f28a5ba9947157dae255e9f0b97b8367e9f60
John Malconian <malc@indexdata.com>
2017-10-16 18:01:00 +0000

    make dockerDir a string instead of list

--- vars/buildJavaModDocker.groovy
+++ vars/buildJavaModDocker.groovy
@@ -5,7 +5,7 @@
  * Send notifications based on build status string
  */
 
-def call(String name, String version, List dockerDir) {
+def call(String name, String version, String dockerDir) {
 
   def fatJar = "${name}-fat.jar"
   def dockerFile
@@ -14,51 +14,49 @@ def call(String name, String version, List dockerDir) {
 
   echo "Fat Jar is: $fatJar"
   
-  for (d in dockerDir) {
-    dir("${env.WORKSPACE}/${d}") 
+  dir("$dockerDir") 
 
-    if (fileExists('.dockerignore')) {
-      echo "Using existing .dockerignore"
-    }
-    else {
-      echo "Creating .dockerignore"
-      sh """
-      cat > .dockerignore << EOF
+  if (fileExists('.dockerignore')) {
+    echo "Using existing .dockerignore"
+  }
+  else {
+    echo "Creating .dockerignore"
+    sh """
+    cat > .dockerignore << EOF
 *
 !Dockerfile
 !docker*
 !target/*.jar
 EOF
-      """
-    }
+    """
+  }
 
-    if (fileExists('Dockerfile')) {
-      echo "Found existing Dockerfile." 
-      buildArg = 'no'
-    }
-    else {
-      echo "Dockerfile not found.  Using Java template."
-      dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
-      writeFile file: 'Dockerfile', text: "$dockerFile"
-      buildArg = 'yes'
-    }
+  if (fileExists('Dockerfile')) {
+    echo "Found existing Dockerfile." 
+    buildArg = 'no'
+  }
+  else {
+    echo "Dockerfile not found.  Using Java template."
+    dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
+    writeFile file: 'Dockerfile', text: "$dockerFile"
+    buildArg = 'yes'
+  }
 
-    if (fileExists('docker/docker-entrypoint.sh')) {
-      echo "Found existing docker-entrypoint.sh script." 
-    }
-    else {
-      echo "docker-entrypoint.sh not found. Using Java entrypoint template."
-      dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
-      writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
-    }
+  if (fileExists('docker/docker-entrypoint.sh')) {
+    echo "Found existing docker-entrypoint.sh script." 
+  }
+  else {
+    echo "docker-entrypoint.sh not found. Using Java entrypoint template."
+    dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
+    writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
+  }
 
-    if (buildArg == 'yes') {
-      sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
-    }
-    else {
-      sh "docker build --tag ${name}:${version} ."
-    }
+  if (buildArg == 'yes') {
+    sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
+  }
+  else {
+    sh "docker build --tag ${name}:${version} ."
+  }
   
-    sh "docker tag ${name}:${version} ${name}:latest"  
-  } 
+  sh "docker tag ${name}:${version} ${name}:latest"  
 }
