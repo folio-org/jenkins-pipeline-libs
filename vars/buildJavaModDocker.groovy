9e1408fc0e1694724238caa60657c325a897e1d2
John Malconian <malc@indexdata.com>
2017-10-16 18:27:44 +0000

    add body for dir()

--- vars/buildJavaModDocker.groovy
+++ vars/buildJavaModDocker.groovy
@@ -14,49 +14,50 @@ def call(String name, String version, String dockerDir) {
 
   echo "Fat Jar is: $fatJar"
   
-  dir("$dockerDir") 
+  dir("$dockerDir") {
 
-  if (fileExists('.dockerignore')) {
-    echo "Using existing .dockerignore"
-  }
-  else {
-    echo "Creating .dockerignore"
-    sh """
-    cat > .dockerignore << EOF
+    if (fileExists('.dockerignore')) {
+      echo "Using existing .dockerignore"
+    }
+    else {
+      echo "Creating .dockerignore"
+      sh """
+      cat > .dockerignore << EOF
 *
 !Dockerfile
 !docker*
 !target/*.jar
 EOF
     """
-  }
+    }
 
-  if (fileExists('Dockerfile')) {
-    echo "Found existing Dockerfile." 
-    buildArg = 'no'
-  }
-  else {
-    echo "Dockerfile not found.  Using Java template."
-    dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
-    writeFile file: 'Dockerfile', text: "$dockerFile"
-    buildArg = 'yes'
-  }
+    if (fileExists('Dockerfile')) {
+      echo "Found existing Dockerfile." 
+      buildArg = 'no'
+    }
+    else {
+      echo "Dockerfile not found.  Using Java template."
+      dockerFile = libraryResource 'org/folio/Dockerfile.javaModule'
+      writeFile file: 'Dockerfile', text: "$dockerFile"
+      buildArg = 'yes'
+    }
 
-  if (fileExists('docker/docker-entrypoint.sh')) {
-    echo "Found existing docker-entrypoint.sh script." 
-  }
-  else {
-    echo "docker-entrypoint.sh not found. Using Java entrypoint template."
-    dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
-    writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
-  }
+    if (fileExists('docker/docker-entrypoint.sh')) {
+      echo "Found existing docker-entrypoint.sh script." 
+    }
+    else {
+      echo "docker-entrypoint.sh not found. Using Java entrypoint template."
+      dockerEntrypoint = libraryResource 'org/folio/docker-entrypoint.javaModule'
+      writeFile file: 'docker-entrypoint.sh', text: "$dockerEntrypoint"
+    }
 
-  if (buildArg == 'yes') {
-    sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
-  }
-  else {
-    sh "docker build --tag ${name}:${version} ."
-  }
-  
-  sh "docker tag ${name}:${version} ${name}:latest"  
+    if (buildArg == 'yes') {
+      sh "docker build --tag ${name}:${version} --build-arg='VERTICLE_FILE=${fatJar}' . "
+    }
+    else {
+      sh "docker build --tag ${name}:${version} ."
+    }
+    sh "docker tag ${name}:${version} ${name}:latest"  
+
+  } // end dir()
 }
