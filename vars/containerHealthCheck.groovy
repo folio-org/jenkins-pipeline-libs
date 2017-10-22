741a735996f03035beed020728c84f169688f037
John Malconian <malc@indexdata.com>
2017-10-22 00:22:03 +0000

    syntax

--- vars/containerHealthCheck.groovy
+++ vars/containerHealthCheck.groovy
@@ -5,15 +5,14 @@ def call(String dockerImage, String checkCmd, String runArgs) {
    def timeout = '2s'
    def retries = 2
    def cidFile = "/tmp/${dockerImage}-${env.BUILD_NUMBER}.cid"
-   def startupWaitRange = 1..40 // seconds
    def maxStartupWait = 40
    def health = ''
 
    try {
      echo "Testing $dockerImage image. Starting container.."
+     echo "docker run -d --health-timeout=${timeout} --health-retries=${retries}  --health-cmd='${checkCmd}' --cidfile $cidFile $dockerImage $runArgs"
      sh """
-     docker run -d --health-timeout=${timeout} --health-retries=${retries} \
-       --health-cmd=${checkCmd} --cidfile $cidFile $dockerImage $runArgs
+     docker run -d --health-timeout=${timeout} --health-retries=${retries}  --health-cmd='${checkCmd}' --cidfile $cidFile $dockerImage $runArgs
      """
    for (i = 0; i <maxStartupWait; i++) {
        health = sh(returnStdout: true, script: 'docker inspect `cat "$cidFile"` | jq -r \".[].State.Health.Status\"').trim()
