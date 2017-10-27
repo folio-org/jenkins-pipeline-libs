fef2a9b64f7b1576f463f2db460b60f02fc2a3fa
John Malconian <malc@indexdata.com>
2017-10-27 17:02:17 +0000

    syntax

--- vars/containerHealthCheck.groovy
+++ vars/containerHealthCheck.groovy
@@ -4,13 +4,12 @@ def call(String dockerImage, String checkCmd, String runArgs) {
    
    def timeout = '2s'
    def retries = 2
-   def dateSec = sh(returnStdout: true, script: 'date +%N').trim()
-   def cidFile = "/tmp/${dateSec}.cid"
+   def cidFile = "${dockerImage}-${env.BUILD_NUMBER}.cid"
    def maxStartupWait = 40
    def health = ''
 
    try {
-     echo "Testing $dockerImage image. Starting container.."
+     echo "Testing $dockerImage image. Starting container..."
 
      // exit 1 since 'docker run' can return a variety of non-zero status codes.
      sh """
@@ -18,11 +17,15 @@ def call(String dockerImage, String checkCmd, String runArgs) {
              --health-cmd='${checkCmd}' --cidfile $cidFile $dockerImage $runArgs || exit 1
      """
       
-     def cid = readFile("$cidFile")
+     def cid = readFile(cidFile)
 
      for (i = 0; i <maxStartupWait; i++) {
-       health = sh(returnStdout: true, script: "docker inspect $cid | jq -r \\".[].State.Health.Status\\"").trim()
+
+       health = sh(returnStdout: true, 
+                   script: "docker inspect $cid | jq -r \".[].State.Health.Status\"").trim()
+
        echo "Current Status: $health"
+
        if (health == 'starting') {
          sleep 1
        }
@@ -32,7 +35,6 @@ def call(String dockerImage, String checkCmd, String runArgs) {
      }
    }
    catch(Exception err) {
-       health = 'error'
        throw err
    }
    finally {
