bcb9e281876d1a5c9981a094e64687decc5308c4
John Malconian <malc@indexdata.com>
2017-10-31 19:56:38 +0000

    typo

--- vars/sonarqubeMvn.groovy
+++ vars/sonarqubeMvn.groovy
@@ -40,7 +40,7 @@ def call(String sqBranch = null) {
 
       if (sqBranch) { 
         withSonarQubeEnv('SonarCloud') {
-          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar" +
+          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " +
                "-Dsonar.organization=folio-org -Dsonar.verbose=true -Dsonar.branch=$sqBranch"
         }
       } 
