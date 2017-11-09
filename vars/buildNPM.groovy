d9c3e78f833cf6a66663c7209b71b45001d97a0d
John Malconian <malc@indexdata.com>
2017-11-09 20:22:36 +0000

    use our original docker node

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -7,8 +7,7 @@ def call(body) {
   body.delegate = config
   body()
 
-//  node('folio-jenkins-slave-docker') {
-  node('jenkins-slave-nodejs') {
+  node('folio-jenkins-slave-docker') {
 
     try {
       stage('Checkout') {
