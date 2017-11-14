a4c8743477a885a2d8526c6a7ccb89109c57446b
John Malconian <malc@indexdata.com>
2017-11-14 18:34:12 +0000

    add source files with common commands

---
+++ src/org/folio/foliociCommands.groovy
@@ -0,0 +1,40 @@
+#!/usr/bin/env groovy
+
+package org.folio
+
+// Update npm package.json version to "snapshot" version for FOLIO CI
+npmSnapshotVersion() {
+
+  def folioci_npmver = libraryResource('org/folio/folioci_npmver.sh')
+  writeFile file: 'folioci_npmver.sh', text: folioci_npmver
+  sh 'chmod +x folioci_npmver.sh'
+  sh 'npm version `./folioci_npmver.sh`'
+
+}
+
+// get the unscoped module name and version from package.json. 
+// Return name:version map.
+npmSimpleNameVersion(String npmPackageFile = 'package.json') {
+  
+  def simpleNameVersion = [:]
+  def json = readJSON(file: 'npmPackageFile')
+  def name = json.name.replaceAll(~/\//, "_")  
+
+  name = name.replaceAll(~/@/, "")  
+  version = json.version
+  
+  simpleNameVersion = [name:version]
+  
+  return simpleNameVersion
+}
+
+// get base repo/project name
+getProjName() {
+
+  def proj_name = sh(returnStdout: true, 
+      script: 'git config remote.origin.url | awk -F \'/\' \'{print $5}\' | sed -e \'s/\\.git//\'').trim()
+
+  return proj_name
+}
+
+
