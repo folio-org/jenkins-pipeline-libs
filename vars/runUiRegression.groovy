28a1c23771bbbde2a4f2aad2b9011f53cbe0827e
John Malconian <malc@indexdata.com>
2017-12-05 20:17:25 +0000

    echo

--- vars/runUiRegression.groovy
+++ vars/runUiRegression.groovy
@@ -9,13 +9,13 @@ def call(String uitestVer, String folioUrl) {
 
   def uitestImage = "folioci/ui-testing:${uitestVer}"
 
-  echo "<html><head><title>UI Regression Test Report</title></head>" > rtest.out
-  echo "<body><pre>" >> rtest.html
+  sh 'echo "<html><head><title>UI Regression Test Report</title></head>" > rtest.html'
+  sh 'echo "<body><pre>" >> rtest.html'
 
   sh "docker pull $uitestImage"
   def testStatus = sh(script: "docker run -i --rm -e \"FOLIO_UI_URL=${folioUrl}\" $uitestImage >> rtest.html 2>&1", returnStatus:true)
 
-  echo "</pre><body></html>" >> rtest.html
+  sh 'echo "</pre><body></html>" >> rtest.html'
 
   publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
                keepAll: true, reportDir: '.', 
