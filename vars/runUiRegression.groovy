6b3da7e1508fbec21b0f71aca499385a1bc8b655
John Malconian <malc@indexdata.com>
2017-12-06 14:58:13 +0000

    set testStatus in shared lib

--- vars/runUiRegression.groovy
+++ vars/runUiRegression.groovy
@@ -8,23 +8,35 @@
 def call(String uitestVer, String folioUrl) {
 
   def uitestImage = "folioci/ui-testing:${uitestVer}"
+  def testStatus = ''
 
   sh 'echo "<html><head><title>UI Regression Test Report</title></head>" > rtest.html'
   sh 'echo "<body><pre>" >> rtest.html'
 
+  echo "Running UI Regression test image $uitestImage against $folioUrl"
   sh "docker pull $uitestImage"
-  def testStatus = sh(script: "docker run -i --rm -e \"FOLIO_UI_URL=${folioUrl}\" $uitestImage >> rtest.html 2>&1", returnStatus:true)
+  def returnStatus = sh(script: "docker run -i --rm -e \"FOLIO_UI_URL=${folioUrl}\" $uitestImage >> rtest.html 2>&1", returnStatus:true)
 
   sh 'echo "</pre><body></html>" >> rtest.html'
  
+  // print test results to job console
   def testReport =  readFile('rtest.html')
   echo "$testReport"
 
+  // publish results
   publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
                keepAll: true, reportDir: '.', 
                reportFiles: 'rtest.html', 
                reportName: 'UI Regression Test Report', 
                reportTitles: 'UI Regression Test Report'])
 
+  // An exit code on non-zero indicates something failed
+  if (returnStatus != 0) { 
+    testStatus = 'FAILED'
+  }
+  else {
+    testStatus = 'SUCCESS'
+  }
+  
   return testStatus
 }
