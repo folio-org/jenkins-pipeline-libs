3eb5920e179230f893f0a918e867850e4581f080
John Malconian <malc@indexdata.com>
2017-12-07 16:26:14 +0000

    add resource shell script to update route53 with latest build names

---
+++ resources/org/folio/route53_update_build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# ec2_group
+group_tag=$1
+
+# 'latest' or 'stable'
+build=$2
+
+base_build=$(awk -F '_' '{ print $1 "-" $2}'<<< $group_tag)
+hostname=$(awk -F '_' '{ print $1 "-" $2 "-" $3}'<<< $group_tag)
+aws="/usr/bin/aws --region us-east-1"
+public_zoneid="Z2F9IQRBHKK7BO"
+private_zoneid="Z3JKLZ9JDZ7HCP"
+
+public_record=$(cat <<EOF
+{
+"Comment": "Update public zone record to reflect latest ${build_base}-${build}",
+  "Changes": [
+    { 
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "${base_build}-${build}.aws.indexdata.com.",
+        "Type": "CNAME",
+        "TTL": 60,
+        "ResourceRecords": [
+          { 
+            "Value": "${hostname}.aws.indexdata.com"
+          }
+        ]
+      }
+   }
+ ]
+}
+EOF
+)
+
+private_record=$(cat <<EOF
+{
+"Comment": "Update internal zone record to reflect latest ${base_build}-${build}",
+  "Changes": [
+    { 
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "${base_build}-${build}.indexdata.internal.",
+        "Type": "CNAME",
+        "TTL": 60,
+        "ResourceRecords": [
+          { 
+            "Value": "${hostname}.indexdata.internal"
+          }
+        ]
+      }
+   }
+ ]
+}
+EOF
+)
+
+echo "Posting CNAME record for ${base_build}-${build}.aws.indexdata.com to AWS route53:" 
+echo "$public_record"
+echo "$public_record" > public_record.out
+$aws route53 --hosted-zone-id $public_zoneid --change-batch file://public_record.out
+
+echo "Posting CNAME record for ${base_build}-${build}.indexdata.internal to AWS route53:" 
+echo "$private_record"
+echo "$private_record" > private_record.out
+$aws route53 --hosted-zone-id $private_zoneid --change-batch file://private_record.out
