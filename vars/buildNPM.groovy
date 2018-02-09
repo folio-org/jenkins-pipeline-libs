5f9b95ed0c9bd6720bc5a7b5c1de950b29655f6e
John Malconian <malc@indexdata.com>
2018-02-09 16:07:19 +0000

    clone ui-testing and folio-testing-platform. cd to 'project'

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -56,130 +56,135 @@ def call(body) {
                  userRemoteConfigs: scm.userRemoteConfigs
          ])
 
+         git 'https://github.com/folio-org/ui-testing'
+         git 'https://github.com/folio-org/folio-testing-platform'
+
          echo "Checked out $env.BRANCH_NAME"
          echo "Workspace: $env.WORKSPACE"
       }
 
-      stage('Prep') {
+      dir('project') {
+        stage('Prep') {
 
-        if (env.snapshot) {
-          foliociLib.npmSnapshotVersion()
-        }
-
-        def Map simpleNameVerMap = foliociLib.npmSimpleNameVersion('package.json')          
-        simpleNameVerMap.each { key, value ->
-          env.simpleName = key
-          env.version = value
-        }
-        echo "Package Simplfied Name: $env.simpleName"
-        echo "Package Version: $env.version"
-
-        // project name is the GitHub repo name and is typically
-        // different from mod name specified in package.json
-        env.project_name = foliociLib.getProjName()
-        echo "Project Name: $env.project_name"
-      }
- 
-      withCredentials([string(credentialsId: 'jenkins-npm-folioci',variable: 'NPM_TOKEN')]) {
-        withNPM(npmrcConfig: 'jenkins-npm-folioci') {
-          stage('NPM Build') {
-            sh 'yarn install' 
+          if (env.snapshot) {
+            foliociLib.npmSnapshotVersion()
           }
 
-          if (config.runLint ==~ /(?i)(Y|YES|T|TRUE)/) {
-            stage('ESLint') {
-              echo "Running ESLint..."
-              def lintStatus = sh(returnStatus:true, script: 'yarn lint 2>/dev/null 1> lint.output')
-              echo "Lint Status: $lintStatus"
-              if (lintStatus != 0) {
-                def lintReport =  readFile('lint.output')
-                if (env.CHANGE_ID) {
-                  // Requires https://github.com/jenkinsci/pipeline-github-plugin
-                  // comment is response to API request in case we ever need it.
-                  def comment = pullRequest.comment(lintReport)
-                  echo "$comment"
+          def Map simpleNameVerMap = foliociLib.npmSimpleNameVersion('package.json')          
+          simpleNameVerMap.each { key, value ->
+            env.simpleName = key
+            env.version = value
+          }
+          echo "Package Simplfied Name: $env.simpleName"
+          echo "Package Version: $env.version"
+
+          // project name is the GitHub repo name and is typically
+          // different from mod name specified in package.json
+          env.project_name = foliociLib.getProjName()
+          echo "Project Name: $env.project_name"
+        }
+ 
+        withCredentials([string(credentialsId: 'jenkins-npm-folioci',variable: 'NPM_TOKEN')]) {
+          withNPM(npmrcConfig: 'jenkins-npm-folioci') {
+            stage('NPM Build') {
+              sh 'yarn install' 
+            }
+
+            if (config.runLint ==~ /(?i)(Y|YES|T|TRUE)/) {
+              stage('ESLint') {
+                echo "Running ESLint..."
+                def lintStatus = sh(returnStatus:true, script: 'yarn lint 2>/dev/null 1> lint.output')
+                echo "Lint Status: $lintStatus"
+                if (lintStatus != 0) {
+                  def lintReport =  readFile('lint.output')
+                  if (env.CHANGE_ID) {
+                    // Requires https://github.com/jenkinsci/pipeline-github-plugin
+                    // comment is response to API request in case we ever need it.
+                    def comment = pullRequest.comment(lintReport)
+                    echo "$comment"
+                  }
+                  else {
+                    echo "$lintReport"
+                  }
                 }
                 else {
-                  echo "$lintReport"
+                  echo "No lint errors found"
+                }
+              }
+            }
+
+            if (config.runTest ==~ /(?i)(Y|YES|T|TRUE)/) {
+              stage('Unit Tests') {
+                echo "Running unit tests..."
+                sh 'yarn test'
+              }
+            }
+
+            if ( env.BRANCH_NAME == 'master' ) {
+              if (npmDeploy ==~ /(?i)(Y|YES|T|TRUE)/) {
+                stage('NPM Deploy') {
+                  // npm is more flexible than yarn for this stage. 
+                  echo "Deploying NPM packages to Nexus repository"
+                  sh 'npm publish -f'
+                }
+              }
+            }
+
+          }  // end withNPM
+          // remove .npmrc put in directory by withNPM
+          sh 'rm -f .npmrc'
+        }  // end WithCred    
+
+        if (config.doDocker) {
+          stage('Docker Build') {
+            // use env.project_name as name of docker artifact
+            env.name = env.project_name
+            echo "Building Docker image for $env.name:$env.version" 
+            config.doDocker.delegate = this
+            config.doDocker.resolveStrategy = Closure.DELEGATE_FIRST
+            config.doDocker.call()
+          }
+        } 
+
+        if ( env.BRANCH_NAME == 'master' ) {
+          if (config.publishModDescriptor ==~ /(?i)(Y|YES|T|TRUE)/) {
+            // We assume that MDs are included in package.json
+            stage('Publish Module Descriptor') {
+              def modDescriptor = ''
+              if (config.modDescriptor) { 
+                modDescriptor = config.modDescriptor
+                env.name = env.project_name
+                if (env.snapshot) {
+                  // update the version to the snapshot version
+                  echo "Update Module Descriptor version to snapshot version"
+                  foliociLib.updateModDescriptorId(modDescriptor)
                 }
               }
               else {
-                echo "No lint errors found"
+                echo "Generating Stripes Module Descriptor from package.json"
+                env.name = env.simpleName
+                sh 'git clone https://github.com/folio-org/stripes-core'
+                sh 'stripes-core/util/package2md.js --strict package.json > ModuleDescriptor.json'
+                modDescriptor = 'ModuleDescriptor.json'
+              }
+              echo "Publishing Module Descriptor to FOLIO registry"
+              postModuleDescriptor(modDescriptor) 
+            }
+          }
+          if (config.publishAPI ==~ /(?i)(Y|YES|T|TRUE)/) {
+            stage('Publish API Docs') {
+              echo "Publishing API docs"
+              sh "python3 /usr/local/bin/generate_api_docs.py -r $env.project_name -v -o folio-api-docs"
+              withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+                   accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                   credentialsId: 'jenkins-aws',
+                   secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                sh 'aws s3 sync folio-api-docs s3://foliodocs/api'
               }
             }
           }
-
-          if (config.runTest ==~ /(?i)(Y|YES|T|TRUE)/) {
-            stage('Unit Tests') {
-              echo "Running unit tests..."
-              sh 'yarn test'
-            }
-          }
-
-          if ( env.BRANCH_NAME == 'master' ) {
-            if (npmDeploy ==~ /(?i)(Y|YES|T|TRUE)/) {
-              stage('NPM Deploy') {
-                // npm is more flexible than yarn for this stage. 
-                echo "Deploying NPM packages to Nexus repository"
-                sh 'npm publish -f'
-              }
-            }
-          }
-
-        }  // end withNPM
-        // remove .npmrc put in directory by withNPM
-        sh 'rm -f .npmrc'
-      }  // end WithCred    
-
-      if (config.doDocker) {
-        stage('Docker Build') {
-          // use env.project_name as name of docker artifact
-          env.name = env.project_name
-          echo "Building Docker image for $env.name:$env.version" 
-          config.doDocker.delegate = this
-          config.doDocker.resolveStrategy = Closure.DELEGATE_FIRST
-          config.doDocker.call()
-        }
-      } 
-
-      if ( env.BRANCH_NAME == 'master' ) {
-        if (config.publishModDescriptor ==~ /(?i)(Y|YES|T|TRUE)/) {
-          // We assume that MDs are included in package.json
-          stage('Publish Module Descriptor') {
-            def modDescriptor = ''
-            if (config.modDescriptor) { 
-              modDescriptor = config.modDescriptor
-              env.name = env.project_name
-              if (env.snapshot) {
-                // update the version to the snapshot version
-                echo "Update Module Descriptor version to snapshot version"
-                foliociLib.updateModDescriptorId(modDescriptor)
-              }
-            }
-            else {
-              echo "Generating Stripes Module Descriptor from package.json"
-              env.name = env.simpleName
-              sh 'git clone https://github.com/folio-org/stripes-core'
-              sh 'stripes-core/util/package2md.js --strict package.json > ModuleDescriptor.json'
-              modDescriptor = 'ModuleDescriptor.json'
-            }
-            echo "Publishing Module Descriptor to FOLIO registry"
-            postModuleDescriptor(modDescriptor) 
-          }
-        }
-        if (config.publishAPI ==~ /(?i)(Y|YES|T|TRUE)/) {
-          stage('Publish API Docs') {
-            echo "Publishing API docs"
-            sh "python3 /usr/local/bin/generate_api_docs.py -r $env.project_name -v -o folio-api-docs"
-            withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
-                 accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                 credentialsId: 'jenkins-aws',
-                 secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-              sh 'aws s3 sync folio-api-docs s3://foliodocs/api'
-            }
-          }
-        }
-      } 
+        } 
+      } // end dir
 
     }  // end try
     catch (Exception err) {
