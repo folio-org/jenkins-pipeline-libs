c4cc43239be0fd14145ca2cd01e88ced9f39103e
John Malconian <malc@indexdata.com>
2018-03-06 19:44:46 +0000

    folio-1043

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -269,7 +269,10 @@ def call(body) {
           } 
 
           dir("${env.WORKSPACE}/ui-testing") {  
-            sh 'yarn install' 
+            withCredentials([string(credentialsId: 'jenkins-npm-folioci',variable: 'NPM_TOKEN')]) {
+              withNPM(npmrcConfig: 'jenkins-npm-folioci') {
+                sh 'yarn install'
+              }
 
             env.FOLIO_UI_USERNAME = "${env.tenant}_admin"
             env.FOLIO_UI_PASSWORD = "admin"
