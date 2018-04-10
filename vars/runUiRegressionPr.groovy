ce466bae624a8085e28bf5ab5620a3d6b81f7f2f
John Malconian <malc@indexdata.com>
2018-04-10 17:06:14 +0000

    non-docker-based script for running ui regression tests

---
+++ vars/runUiRegressionPr.groovy
@@ -0,0 +1,54 @@
+#!/usr/bin/env groovy
+
+
+/*
+ * Run UI Regression tests
+ */
+
+def call(String folioUser, String folioPassword, String folioUrl) {
+
+  def testStatus = ''
+
+  withCredentials([string(credentialsId: 'jenkins-npm-folioci',variable: 'NPM_TOKEN')]) {
+    withNPM(npmrcConfig: 'jenkins-npm-folioci') {
+      // I think we need to remove the yarn lock file?
+      sh 'rm -f yarn.lock'
+      sh 'yarn install' 
+      sh '/usr/bin/Xvfb :2 &'
+      sh 'sleep 1'
+
+      env.FOLIO_UI_USERNAME = folioUser
+      env.FOLIO_UI_PASSWORD = folioPassword
+      env.FOLIO_UI_URL = folioUrl
+
+      sh 'echo "<html><head><title>UI Regression Test Report</title></head>" > rtest.html'
+      sh 'echo "<body><pre>" >> rtest.html'
+
+      echo "Running UI Regression test against $folioUrl"
+      def returnStatus = sh(script: "DISPLAY=:2 yarn test >> rtest.html 2>&1", returnStatus:true)
+
+      sh 'echo "</pre><body></html>" >> rtest.html'
+    }
+  }
+ 
+  // print test results to job console
+  def testReport =  readFile('rtest.html')
+  echo "$testReport"
+
+  // publish results
+  publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
+               keepAll: true, reportDir: '.', 
+               reportFiles: 'rtest.html', 
+               reportName: 'UI Regression Test Report', 
+               reportTitles: 'UI Regression Test Report'])
+
+  // An exit code on non-zero indicates something failed
+  if (returnStatus != 0) { 
+    testStatus = 'FAILED'
+  }
+  else {
+    testStatus = 'SUCCESS'
+  }
+  
+  return testStatus
+}
