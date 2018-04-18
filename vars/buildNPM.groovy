95694185df42181ab76f3aed00a16e9ccf39da02
John Malconian <malc@indexdata.com>
2018-04-18 19:38:43 +0000

    test post-merge-2

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -197,8 +197,8 @@ def call(body) {
          ( runRegression ==~ /(?i)(Y|YES|T|TRUE)/)) {
 
         // ensure tenant id is unique
-        //def tenant = "${env.BRANCH_NAME}_${env.BUILD_NUMBER}"
-        def tenant = "${env.CHANGE_ID}_${env.BUILD_NUMBER}"
+        def tenant = "${env.BRANCH_NAME}_${env.BUILD_NUMBER}"
+        //def tenant = "${env.CHANGE_ID}_${env.BUILD_NUMBER}"
         env.tenant = foliociLib.replaceHyphen(tenant)
         env.okapiUrl = 'http://folio-snapshot-stable.aws.indexdata.com:9130'
 
