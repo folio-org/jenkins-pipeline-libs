ac906cd8fe5fe1b502b71872c71244536a279acb
John Malconian <malc@indexdata.com>
2018-04-27 19:22:25 +0000

    rearrange conditionals

--- vars/runLintNPM.groovy
+++ vars/runLintNPM.groovy
@@ -5,8 +5,7 @@ def call() {
     sh 'mkdir -p ci'
     def lintStatus = sh(returnStatus:true, script: 'yarn lint 2>/dev/null 1> ci/lint.html')
     def lintReport = readFile('ci/lint.html')
-    echo "$lintReport"
-  
+
     publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
                  keepAll: true, reportDir: 'ci',
                  reportFiles: 'lint.html',
@@ -15,15 +14,17 @@ def call() {
 
     sh 'rm -rf ci'
 
-    // PR
-    if (env.CHANGE_ID) {
-      if (lintStatus != 0) { 
+    if (lintStatus != 0) { 
+      echo "Lint errors detected:"
+      echo "$lintReport"
+      // PR
+      if (env.CHANGE_ID) {
         // Requires https://github.com/jenkinsci/pipeline-github-plugin
         pullRequest.comment(lintReport) 
       }
-      else {
-        pullRequest.commend('No lint errors detected')
-      }
+    }
+    else {
+      echo "No lint errors detected."
     }
   }
 }
