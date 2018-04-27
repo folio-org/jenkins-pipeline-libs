14e09413bdc5d5acf836a2ad1db155cd7f98d381
John Malconian <malc@indexdata.com>
2018-04-27 18:38:25 +0000

    runLintNPM.groovy

---
+++ vars/runLintNPM.groovy
@@ -0,0 +1,30 @@
+
+def call() { 
+  stage('Lint') { 
+    echo "Running 'yarn lint...'"
+    sh 'mkdir -p ci'
+    def lintStatus = sh(returnStatus:true, script: 'yarn lint 2>/dev/null 1> ci/lint.html'
+    def lintReport = readFile('lint.html')
+    echo "$lintReport"
+  
+    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
+                 keepAll: true, reportDir: 'ci',
+                 reportFiles: 'lint.html',
+                 reportName: 'Lint',
+                 reportTitles: 'Lint'])
+
+    sh 'rm -rf ci'
+
+    // PR
+    if (env.CHANGE_ID) {
+      if (lintStatus != 0) { 
+        // Requires https://github.com/jenkinsci/pipeline-github-plugin
+        pullRequest.comment(lintReport) 
+      }
+      else {
+        pullRequest.commend('No lint errors detected')
+      }
+    }
+  }
+}
+
