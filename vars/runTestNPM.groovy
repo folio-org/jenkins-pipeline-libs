d82da365700fa0a4a275ab5f71a604f057e89333
John Malconian <malc@indexdata.com>
2018-05-03 13:05:30 +0000

    get env right

--- vars/runTestNPM.groovy
+++ vars/runTestNPM.groovy
@@ -21,46 +21,48 @@ def call(String runTestOptions = '') {
 
     // start Xvfb for tests that require browsers/displays
     sudo Xvfb :20 &
-    env.DISPLAY = ':20'
 
-    // set karma env for browsers
-    env.CHROME_BIN = /usr/bin/google-chrome-stable
-    env.FIREFOX_BIN = /usr/bin/firefox
+    withEnv([ 
+      'DISPLAY=:20',
+      'CHROME_BIN=/usr/bin/google-chrome-stable',
+      'FIREFOX_BIN=/usr/bin/google-chrome-stable'
+    ]) { 
 
-    echo "Local browsers available:"
-    sh "$CHROME_BIN --version"
-    sh "$FIREFOX_BIN --version"
+      echo "Local browsers available:"
+      sh "$CHROME_BIN --version"
+      sh "$FIREFOX_BIN --version"
 
-    sh 'echo "<html><body><pre>" > ci/test.html'
+      sh 'echo "<html><body><pre>" > ci/test.html'
 
-    def testStatus = sh(returnStatus:true, script: "yarn run $runTestOptions 2>/dev/null 1>> ci/test.html")
+      def testStatus = sh(returnStatus:true, script: "yarn run $runTestOptions 2>/dev/null 1>> ci/test.html")
 
-    sh 'echo "</pre><body></html>" >> ci/test.html'
+      sh 'echo "</pre><body></html>" >> ci/test.html'
  
-    def testReport = readFile('ci/test.html')
-    echo "$testReport"
+      def testReport = readFile('ci/test.html')
+      echo "$testReport"
     
-    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
+      publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
                  keepAll: true, reportDir: 'ci',
                  reportFiles: 'test.html',
                  reportName: 'Test',
                  reportTitles: 'Test'])
 
-    sh 'rm -rf ci'
+      sh 'rm -rf ci'
 
-    if (testStatus != 0) { 
-      def message = "Test errors found. $testReportUrl"
-      // PR
-      if (env.CHANGE_ID) {
-        // Requires https://github.com/jenkinsci/pipeline-github-plugin
-        @NonCPS
-        def comment = pullRequest.comment(message) 
+      if (testStatus != 0) { 
+        def message = "Test errors found. $testReportUrl"
+        // PR
+        if (env.CHANGE_ID) {
+          // Requires https://github.com/jenkinsci/pipeline-github-plugin
+          @NonCPS
+          def comment = pullRequest.comment(message) 
+        }
+        error(message)
+
       }
-      error(message)
-
-    }
-    else {
-      echo "All tests completed successfully."
-    }
-  } // end stage
+      else {
+        echo "All tests completed successfully."
+      }
+    } 
+  } // end stage 
 }
