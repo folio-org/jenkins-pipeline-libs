60dd278d1ec071ab1590fe63feee86a58250a529
John Malconian <malc@indexdata.com>
2018-05-04 17:51:14 +0000

    inject karma config and use junit for reporting karma

--- vars/runTestNPM.groovy
+++ vars/runTestNPM.groovy
@@ -17,46 +17,40 @@ def call(String runTestOptions = '') {
 
   stage('Run Local Tests') {
 
-    def testReportUrl = "${env.BUILD_URL}Yarn_20Test_20Report/"
-
     // start Xvfb for tests that require browsers/displays
     sh 'sudo Xvfb :20 &'
 
     withEnv([ 
       'DISPLAY=:20',
       'CHROME_BIN=/usr/bin/google-chrome-stable',
-      'FIREFOX_BIN=/usr/bin/firefox'
+      'FIREFOX_BIN=/usr/bin/firefox',
+      'DEBIAN_FRONTEND=noninteractive'
     ]) { 
 
+      // get latest versions for browsers
+      sh 'apt-get -q update'
+      sh 'apt-get -y --no-install-recommends install google-chrome-stable'
+      sh 'apt-get -y --no-install-recommends install firefox'
+
+      // display available browsers/version
       sh "$CHROME_BIN --version"
       sh "$FIREFOX_BIN --version"
 
+      // inject karma config for karma testing
+      def karmaConf = libraryResource('org/folio/karma.conf.js.ci')
+      writeFile file: 'karma.conf.js', text: $karmaConf
 
-      def testStatus = sh(returnStatus:true, script: "yarn test $runTestOptions 2>&1> test.out")
+      def testStatus = sh(returnStatus:true, script: "yarn test $runTestOptions")
 
-      // encode markup in output that will mess up rendering
-      sh """
-        sed -i -e 's/</\\&lt;/g' -e 's/>/\\&gt;/g' test.out
-      """
+      // publish junit tests if available
+      junit allowEmptyResults: true, testResults: 'runTest/*.xml'
 
-      def testOut = readFile('test.out').trim()
-      echo "$testOut"
-
-      sh 'mkdir -p ci'
-      sh 'echo "<html><body><pre>" > ci/test.html'
-      sh 'cat test.out >> ci/test.html'
-      sh 'echo "</pre><body></html>" >> ci/test.html'
-    
-      publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
-                 keepAll: true, reportDir: 'ci',
-                 reportFiles: 'test.html',
-                 reportName: 'Yarn Test Report',
-                 reportTitles: 'Yarn Test Report'])
-
-      sh 'rm -rf ci'
+      // cleanup CI stuff
+      sh 'rm -rf runTest'
+      sh 'rm -f karma.conf.js'
 
       if (testStatus != 0) { 
-        def message = "Test errors found. $testReportUrl"
+        def message = "Test errors found. See ${env.BUILD_URL}"
         // PR
         if (env.CHANGE_ID) {
           // Requires https://github.com/jenkinsci/pipeline-github-plugin
@@ -64,7 +58,6 @@ def call(String runTestOptions = '') {
           def comment = pullRequest.comment(message) 
         }
         error(message)
-
       }
       else {
         echo "All tests completed successfully."
