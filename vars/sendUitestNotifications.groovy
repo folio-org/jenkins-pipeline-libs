05a9bc05a7ea76d16d4984870f9c50e1224dd0c2
John Malconian <malc@indexdata.com>
2018-05-16 18:55:25 +0000

    revert back to sending okapiUrl

--- vars/sendUitestNotifications.groovy
+++ vars/sendUitestNotifications.groovy
@@ -5,7 +5,7 @@
  * Send notifications based on build status string
  */
 
-def call(String testStatus, String instance, String folioUrl) {
+def call(String testStatus, String instance, String okapiUrl) {
 
   // Default values
   def mailrcpt = 'folio-jenkins@indexdata.com'
@@ -50,7 +50,7 @@ def call(String testStatus, String instance, String folioUrl) {
 
     snsPublish(topicArn:'arn:aws:sns:us-east-1:579891902283:Folio-Environment',
                subject:'FOLIO Regression Test Status',
-               message: "{ \"name\": \"${instance}\", \"message\": \"$summary\", \"folioUrl\": \"$folioUrl\" }",
+               message: "{ \"name\": \"${instance}\", \"message\": \"$summary\", \"okapiUrl\": \"$okapiUrl\" }",
                messageAttributes: ['k1': 'v1', 'k2': 'v2'])
   }
 
