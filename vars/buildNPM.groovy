fa617cb72d194a0c5e72f93dfcf78be87d683ffb
John Malconian <malc@indexdata.com>
2018-06-25 21:03:16 +0000

    remove setting of NODE_OPTIONS

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -78,8 +78,6 @@ def call(body) {
       dir("${env.WORKSPACE}/project") {
         stage('Prep') {
 
-          env.NODE_OPTIONS="--max-old-space-size=2048"
-
           if (env.snapshot) {
             foliociLib.npmSnapshotVersion()
           }
@@ -208,18 +206,6 @@ def call(body) {
         //def okapiUrl = 'http://folio-snapshot-stable.aws.indexdata.com:9130'
         def okapiUrl = 'http://folio-snapshot-test.aws.indexdata.com:9130'
         env.PGHOST = 'folio-snapshot-test.aws.indexdata.com'
-
-        /*
-        * dir("${env.WORKSPACE}/project") {
-        *  // clean up previous 'yarn install'
-        *  sh 'rm -rf node_modules yarn.lock'
-        *  sh 'yarn link'
-        *  // A bit of NPM voodoo. Link to project itself so as not to install
-        *  // package from NPM repository.
-        *  sh "yarn link $env.npmName"
-        *  sh 'yarn install'
-        * }
-        */
 
         // Build stripes, deploy tenant on backend, run ui regression
         buildStripes("$okapiUrl","$tenant",env.stripesPlatform)
