ff316a77dd121e285261f4a90e2ce750fc93a0b9
David Crossley <david@indexdata.com>
2018-07-07 15:07:58 +1000

    Remove trailing spaces FOLIO-351

--- vars/runLintRamlCop.groovy
+++ vars/runLintRamlCop.groovy
@@ -1,13 +1,11 @@
 #!/usr/bin/env groovy
 
-
 /*
  * Run 'yarn lint' on UI modules
  */
 
-
-def call() { 
-  stage('Lint') { 
+def call() {
+  stage('Lint') {
     echo "Running 'yarn lint...'"
     sh 'mkdir -p ci'
     sh 'echo "<html><body><pre>" > ci/lint.html'
@@ -15,11 +13,10 @@ def call() {
     def lintStatus = sh(returnStatus:true, script: 'yarn lint 2>&1 >>  ci/lint.html')
 
     sh 'echo "</pre><body></html>" >> ci/lint.html'
- 
-    def lintReport = readFile('ci/lint.html')
-    
 
-    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, 
+    def lintReport = readFile('ci/lint.html')
+
+    publishHTML([allowMissing: false, alwaysLinkToLastBuild: false,
                  keepAll: true, reportDir: 'ci',
                  reportFiles: 'lint.html',
                  reportName: 'Lint Report',
@@ -27,14 +24,14 @@ def call() {
 
     sh 'rm -rf ci'
 
-    if (lintStatus != 0) { 
+    if (lintStatus != 0) {
       echo "Lint errors detected:"
       echo "$lintReport"
       // PR
       if (env.CHANGE_ID) {
         // Requires https://github.com/jenkinsci/pipeline-github-plugin
         @NonCPS
-        def comment = pullRequest.comment(lintReport) 
+        def comment = pullRequest.comment(lintReport)
       }
     }
     else {
