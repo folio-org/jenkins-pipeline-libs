e6c84957c086ec64d1c736e5de10cb1cffac400c
John Malconian <malc@indexdata.com>
2018-09-12 16:19:34 +0000

    add additional status check when enabling tenant mods

--- vars/deployTenant.groovy
+++ vars/deployTenant.groovy
@@ -30,47 +30,48 @@ def call(String okapiUrl, String tenant) {
     // create tenant
     sh "${scriptPath}/createTenant.sh $okapiUrl $tenant"
 
-    // post MDs and enable tenant modules
+    // post MDs and enable tenant modules. get status
     def mdStatus = sh(script: "${scriptPath}/createTenantModuleList.sh $okapiUrl " +
                         "$tenant ${env.WORKSPACE}/artifacts/md " +
                         "> tenant_mod_list", returnStatus: true)
 
-    if (mdStatus == 0)  { 
-      sh "${scriptPath}/enableTenantModules.sh $okapiUrl $tenant < tenant_mod_list"
+    if (mdStatus != 0)  { 
+      echo "Unable to generate tenant module list"
+      return 1
+    }
+    else {
+      // enable modules for tenant.  get status
+      def enableStatus = sh(script: "${scriptPath}/enableTenantModules.sh $okapiUrl $tenant " +
+                            "< tenant_mod_list", returnStatus: true)
+      if (enableStatus != 0) {
+        echo "Unable to enable all modules for tenant."
+        return 1
+      }
+      else {
+        // set vars in include file
+        sh "echo --- > vars_pr.yml"
+        sh "echo okapi_url: $okapiUrl >> vars_pr.yml"
+        sh "echo tenant: $tenant >> vars_pr.yml"
+        sh "echo admin_user: { username: ${tenant}_admin, password: admin, " +
+           "first_name: Admin, " +
+           "last_name: ${tenant}, " +
+           "email: admin@example.org } >> vars_pr.yml"
+        sh 'echo num_users: 30 >> vars_pr.yml'
 
-      // THE escape sequence from HELL
-      def permissions = '\\\"\\\\\\\\\\\\\\\"perms.all\\\\\\\\\\\\\\\",\\\\\\\\\\\\\\\"login.all\\\\\\\\\\\\\\\",\\\\\\\\\\\\\\\"users.all\\\\\\\\\\\\\\\"\\\"'
-
-      // set vars in include file
-      sh "echo --- > vars_pr.yml"
-      sh "echo okapi_url: $okapiUrl >> vars_pr.yml"
-      sh "echo tenant: $tenant >> vars_pr.yml"
-      sh "echo admin_user: { username: ${tenant}_admin, password: admin, " +
-         "hash: 52DCA1934B2B32BEA274900A496DF162EC172C1E, " +
-         "salt: 483A7C864569B90C24A0A6151139FF0B95005B16, " +
-         "permissions: ${permissions}, " +
-         "first_name: Admin, " +
-         "last_name: ${tenant}, " +
-         "email: admin@example.org } >> vars_pr.yml"
-      sh 'echo num_users: 30 >> vars_pr.yml'
-
-      withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+        withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
                            accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                            credentialsId: 'jenkins-aws',
                            secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
 
-        ansiblePlaybook credentialsId: '11657186-f4d4-4099-ab72-2a32e023cced',
+          ansiblePlaybook credentialsId: '11657186-f4d4-4099-ab72-2a32e023cced',
                           installation: 'Ansible',
                           inventory: 'inventory',
                           playbook: 'folioci-pr.yml',
                           sudoUser: null, vaultCredentialsId: 'ansible-vault-pass'
-      }
+        }
 
-      return 0
-    }
-    else { 
-      echo "Unable to enable all modules for tenant."
-      return 1
+        return 0
+      }
     }
   }    // end dir
 } 
