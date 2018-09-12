58a8a40b325aa27d0a2321ee3daa51442134751c
John Malconian <malc@indexdata.com>
2018-09-12 20:35:42 +0000

    syntax

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -135,7 +135,7 @@ def call(body) {
 
         // add check to ensure git tag and NPM version match if release
         if (env.isRelease) {
-          if ( !foliociLib.tagMatch(env.version) {
+          if ( !foliociLib.tagMatch(env.version) ) {
              error('Git release tag and Maven version mismatch')
           }
         }
