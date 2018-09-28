85dc1a3f27d58ad713827d4b47fbe30ca9ff5537
John Malconian <malc@indexdata.com>
2018-09-28 18:58:31 +0000

    scan on PRs and master repos

--- vars/sonarqubeScanNPM.groovy
+++ vars/sonarqubeScanNPM.groovy
@@ -6,25 +6,42 @@
  */
 
 
-def call(String lcovPath ) {
+def call(String lcovPath = 'artifacts/coverage') {
   withCredentials([[$class: 'StringBinding', 
-                        credentialsId: 'folio-sonar-github-token', 
+                        credentialsId: '6b0ebf62-3a12-4e6b-b77e-c45817b5791b', 
                         variable: 'GITHUB_ACCESS_TOKEN']]) {
     withSonarQubeEnv('SonarCloud') {
-      echo "Performing SonarQube scan" 
       def scannerHome = tool 'SonarQube Scanner'
-      sh """
-      ${scannerHome}/bin/sonar-scanner \
-          -Dsonar.projectKey=folio-org:${env.projectName} \
-          -Dsonar.projectName=${env.projectName} \
-          -Dsonar.projectVersion=${env.version} \
-          -Dsonar.sources=. \
-          -Dsonar.language=js \
-          -Dsonar.exclusions=**/docs/**,**/node_modules/**,**/artifacts/**,**/ci/**,Jenkinsfile,**/LICENSE,**/*.css,**/*.md,**/*.json,**/tests/**/*-test.js,**/stories/*.js **/.stories.js \
-          -Dsonar.javascript.lcov.reportPaths=${lcovPath}/lcov.info \
-          -Dsonar.organization=folio-org 
-      """
-    }
-  } 
+      def excludeFiles = '**/docs/**,**/node_modules/**,**/artifacts/**,**/ci/**,Jenkinsfile,**/LICENSE,**/*.css,**/*.md,**/*.json,**/tests/**/*-test.js,**/stories/*.js **/.stories.js'
+
+      if (env.CHANGE_ID) {
+        sh "${scannerHome}/bin/sonar-scanner " +
+          "-Dsonar.projectKey=folio-org:${env.projectName} " +
+          "-Dsonar.organization=folio-org " +
+          "-Dsonar.sources=. " +
+          "-Dsonar.language=js " +
+          "-Dsonar.verbose=true " +
+          "-Dsonar.exclusions=${excludeFiles} " +
+          "-Dsonar.pullrequest.base=master " +
+          "-Dsonar.pullrequest.key=${env.CHANGE_ID} " +
+          "-Dsonar.pullrequest.branch=${env.BRANCH_NAME} " +
+          "-Dsonar.pullrequest.provider=GitHub " + 
+          "-Dsonar.pullrequest.github.repository=folio-org/${env.projectName} " + 
+          "-Dsonar.pullrequest.github.endpoint=https://api.github.com"
+      }
+      else {  
+        if (env.BRANCH_NAME == 'master') {
+          sh "${scannerHome}/bin/sonar-scanner " +
+            "-Dsonar.organization=folio-org " +
+            "-Dsonar.projectKey=folio-org:${env.projectName} " +
+            "-Dsonar.sources=. " +
+            "-Dsonar.language=js " +
+            "-Dsonar.exclusions=${excludeFiles} " +
+            "-Dsonar.javascript.lcov.reportPaths=${lcovPath}/lcov.info" 
+        }
+      }
+
+    } // end withSonarQubeenv
+  } // end withCredentials
 }
 
