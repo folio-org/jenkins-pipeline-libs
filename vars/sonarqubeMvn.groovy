26cd79c03d27769c3bfaaf1c125df82627349fbc
John Malconian <malc@indexdata.com>
2018-11-14 18:16:14 +0000

    update sonar scanner version and settings for Maven

--- vars/sonarqubeMvn.groovy
+++ vars/sonarqubeMvn.groovy
@@ -14,6 +14,9 @@
 
 def call(String sqBranch = null) {
 
+  def sonarMvnPluginVer = '3.5.0.1254' 
+  // def sonarMvnPluginVer = '3.3.0.603'
+
   if (env.CHANGE_ID) {
     stage('SonarQube Analysis') {
       echo "PR request: $env.CHANGE_ID"
@@ -21,12 +24,14 @@ def call(String sqBranch = null) {
                         credentialsId: '6b0ebf62-3a12-4e6b-b77e-c45817b5791b', 
                         variable: 'GITHUB_ACCESS_TOKEN']]) {
         withSonarQubeEnv('SonarCloud') {
-          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " +
+          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:${sonarMvnPluginVer}:sonar " +
                   "-Dsonar.organization=folio-org -Dsonar.verbose=true " +
-                  "-Dsonar.analysis.mode=preview " +
-                  "-Dsonar.github.pullRequest=${env.CHANGE_ID} " +
-                  "-Dsonar.github.repository=folio-org/${env.projectName} " +
-                  "-Dsonar.github.oauth=${GITHUB_ACCESS_TOKEN}"
+                  "-Dsonar.pullrequest.base=master " +
+                  "-Dsonar.pullrequest.branch=${env.BRANCH_NAME} " +
+                  "-Dsonar.pullrequest.key=${env.CHANGE_ID} " +
+                  "-Dsonar.pullrequest.provider=github " + 
+                  "-Dsonar.pullrequest.github.repository=folio-org/${env.projectName} " +
+                  "-Dsonar.pullrequest.github.endpoint=https://api.github.com"
         }
       }  
     }
@@ -35,19 +40,10 @@ def call(String sqBranch = null) {
     if (env.BRANCH_NAME == 'master') {
       stage('SonarQube Analysis') {
         withSonarQubeEnv('SonarCloud') {
-          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " +
+          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:${sonarMvnPluginVer}:sonar " +
                "-Dsonar.organization=folio-org -Dsonar.verbose=true"
         }
       }
     }
-
-    if (sqBranch) { 
-      stage('SonarQube Analysis') {
-        withSonarQubeEnv('SonarCloud') {
-          sh "mvn -B org.sonarsource.scanner.maven:sonar-maven-plugin:3.3.0.603:sonar " +
-               "-Dsonar.organization=folio-org -Dsonar.verbose=true -Dsonar.branch=$sqBranch"
-        }
-      }
-    } 
   } // end if
 }
