47c1b7c1211ee36d13576108dfa943c5f7548396
Ian Hardy <ian.c.hardy@gmail.com>
2019-04-18 13:34:09 -0400

    use bash steps instead

--- vars/checkStripesDupes.groovy
+++ vars/checkStripesDupes.groovy
@@ -10,32 +10,32 @@
  */
 
 def call(String yarnLockFile) {
-  stage('Check yarn.lock for stripes-* duplicates') {
-    def yarnLock = new File(yarnLockFile)
-    def lines = yarnLock.readLines()
-    def stripesPkgs = []
+  stage('checkDupes') {
+
+    // set default condition to failed
+    def status = 1
+    sh "mkdir -p dupes" 
+
+    sh "grep -oP '^\"\K@folio\/stripes-[^@]*' yarn.lock > dupes/stripes_deps.txt"
+    sh "cat stripes_deps | sort | uniq -d > dupes/stripes_duplicates.txt"
+    
+    status = sh(script:'''
+    if [ -s dupes/stripes_duplicates.txt ]
+    then
+      echo "duplicates found"
+      while read dep; do
+        yarn why $dep
+      done < dupes/stripes_duplicates.txt
+      exit 1
+    else
+      echo "No duplicates found"
+      exit 0
+    fi
+    ''', returnStatus: true)
+    
+    sh "rm -rf dupes"
   
-    //parse yarn.lock for matches to @folio/stripes-*
-    for (line in lines) {
-      if (line =~ /^"(@folio\/stripes-.*@).*\d":$/) {
-        def matcher = (line =~ /^"(@folio\/stripes-.*)@.*\d":$/)
-        stripesPkgs.add(matcher[0][1])
-      }
-    }
-    def duplicates = false //initialize default condition
-    // check for duplicates
-    stripesPkgs.sort()
-    def count = 1 
-    while (count < stripesPkgs.size()) {
-      if (stripesPkgs[count] == stripesPkgs[count -1]) {
-        println stripesPkgs[count] + " is a duplicate"
-        sh "yarn why ${stripesPkgs[count]}"
-        duplicates = true
-      }
-      count += 1
-    }
-  
-    if (duplicates == true) {
+    if (status != 0) {
       def message = "Duplicate stripes-* dependencies found. See ${env.BUILD_URL}"
       // PR
       if (env.CHANGE_ID) {
