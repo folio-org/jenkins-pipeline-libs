151335ab4d7c343bfff3f91a2c154eb1ae2e6a58
Mark Stacy <mbstacy@gmail.com>
2019-06-06 11:50:43 -0600

    Min documentation is Module description, Code Repo Url,Metadata only added if present in Metadata

--- resources/org/folio/dockerHubPublishMetadata.sh
+++ resources/org/folio/dockerHubPublishMetadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SET VARIABLES
+# Script Args: IMAGE VARIABLES
 IMAGE=$1
 REPO_TITLE=$(echo $2 | sed -e 's/-/ /g' -e 's/\b\(.\)/\u\1/g')
 GITHUB_URL=$3
@@ -9,28 +9,38 @@ DOCKER_HUB_TOKEN=$(curl -s -X POST \
     -d '{"username": "'"$DOCKER_USERNAME"'", "password": "'"$DOCKER_PASSWORD"'"}' \
     https://hub.docker.com/v2/users/login/ | jq -r .token)
 
-#PULL MetaData from Mod Descriptor
+#Pull Description from Github API
+GITHUB_API_URL=$(echo $GITHUB_URL | sed -e 's/github.com/api.github.com\/repos/g') 
+GITHUB_API_METADATA=$(curl -s "$GITHUB_API_URL")
+DESCRIPTION=$(echo $GITHUB_API_METADATA | jq '.description' | cut -d "\"" -f 2)
+
+#PULL Metadata from Module Descriptor
 MD_FILE="$WORKSPACE/descriptors/ModuleDescriptor-template.json"
+METADATA=""
 if test -f "$MD_FILE"; then
-    CONTAINER_MEMORY=$(cat $MD_FILE | jq '.metadata.containerMemory' | cut -d "\"" -f 2)
+    CONTAINER_PORT=$(cat $MD_FILE | jq '.launchDescriptor.dockerArgs.HostConfig.PortBindings'| jq 'keys'[0] |  cut -c1-5 | cut -d "\"" -f 2)
+    [ "$CONTAINER_PORT" == null ] && : || METADATA="${METADATA}1. Module Port: $CONTAINER_PORT\n"
     DB_CONNECTION=$(cat $MD_FILE | jq '.metadata.databaseConnection' | cut -d "\"" -f 2)
-else
-    printf "Unable to find push ModuleDescriptor-template.json file"
-    exit 1
+    [ "$DB_CONNECTION" == null ] && : || METADATA="${METADATA}1. Database Connection: $DB_CONNECTION\n"
+    CONTAINER_MEMORY=$(cat $MD_FILE | jq '.metadata.containerMemory' | cut -d "\"" -f 2)
+    [ "$CONTAINER_MEMORY" == null ] && : || METADATA="${METADATA}1. Minimum Memory (MiB): $CONTAINER_MEMORY\n"
+
+    #Set Markdown Metadata Header if needed
+    [ "$METADATA" == "" ] && : || METADATA="### Metadata\n\n${METADATA}"
+    METADATA=$(echo -e $METADATA)
 fi
 
 # SET Docker Hub Markdown Snippet
 read -r -d '' DH_MD_SNIPPIT <<- EOM
-# FOLIO $REPO_TITLE 
+# FOLIO - $REPO_TITLE 
+
+### Description
 
 $DESCRIPTION 
 
-Github Repository: [$GITHUB_URL]($GITHUB_URL). 
+Code Repository: [$GITHUB_URL]($GITHUB_URL) 
 
-# Requirements 
-
-1. Database Connection: $DB_CONNECTION 
-2. Minimum Memory (MiB): $CONTAINER_MEMORY 
+$METADATA 
 
 EOM
 
