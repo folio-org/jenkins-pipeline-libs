ce74ef82a8511ead464787140aa1b583126a4868
Ian Hardy <ian.c.hardy@gmail.com>
2019-08-26 06:45:29 -0400

    point to master branch of folio infra

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -10,7 +10,7 @@ def call(String namespace, String targetModule) {
   
   echo "clone folio-infrastructre"
   checkout([$class: 'GitSCM', 
-            branches: [[name: 'refs/heads/kube-module-update']],
+            branches: [[name: '*/master']],
             doGenerateSubmoduleConfigurations: false, 
             extensions: [[$class: 'SubmoduleOption', 
             disableSubmodules: false, 
