07803cfb29bbc93f046513ccdf03de00c39af21c
Ian Hardy <ian.c.hardy@gmail.com>
2019-09-11 14:14:55 -0400

    point back to master

--- vars/kubeDeploy.groovy
+++ vars/kubeDeploy.groovy
@@ -10,8 +10,7 @@ def call(String namespace, String targetModule) {
   
   echo "clone folio-infrastructre"
   checkout([$class: 'GitSCM', 
-            //branches: [[name: '*/master']],
-            branches: [[name: 'refs/heads/folio-2256']],
+            branches: [[name: '*/master']],
             doGenerateSubmoduleConfigurations: false, 
             extensions: [[$class: 'SubmoduleOption', 
             disableSubmodules: false, 
