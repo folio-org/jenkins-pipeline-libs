0c987f27639c922be98cd98af69354edc08194d7
Ian Hardy <ian.c.hardy@gmail.com>
2019-10-06 14:03:46 -0400

    update preview id

--- vars/buildJavaDocker.groovy
+++ vars/buildJavaDocker.groovy
@@ -141,7 +141,7 @@ EOF
         }
       } else if (env.CHANGE_ID && publishPreview) {
         echo "Publishign Preview Docker images"
-        def previewId = "${env.version}-${env.CHANGE_ID}-${env.BUILD_NUMBER}"
+        def previewId = "${env.version}.${env.CHANGE_ID}"
         docker.withRegistry('https://docker-registry.ci.folio.org/v2/', 'jenkins-nexus')  {
           sh "docker tag ${env.name}:${env.version} docker-registry.ci.folio.org/${env.name}:${previewId}"
           sh "docker push docker-registry.ci.folio.org/${env.name}:${previewId}"
