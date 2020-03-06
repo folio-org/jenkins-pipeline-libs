75cc7f41a69e996e8e22da0e6dc114defd7ce23f
Ian Hardy <ian.c.hardy@gmail.com>
2020-03-05 20:49:29 -0500

    sketch for module cleanup

---
+++ vars/moduleCleanup.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+/* 
+ * cleanup modules from kubernetes
+ */
+
+def call(String namespace, String targetModule) {
+
+  dir("${env.WORKSPACE}") {
+    echo "clone tools"
+    echo "install deps"
+
+    writeFile file: 'requirements.txt', text: libraryResource('org/folio/requirements.txt')
+    sh "pip3 -q install wheel"
+    sh "pip3 -w install requirements.txt"
+    sh "pip3 freeze"
+
+  }
+}
