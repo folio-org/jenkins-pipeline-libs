c4d64363a337845fa5bd01c6842f9c8f2f0df854
Ian Hardy <ian.c.hardy@gmail.com>
2020-03-17 21:21:47 -0400

    test creds

--- vars/moduleCleanup.groovy
+++ vars/moduleCleanup.groovy
@@ -7,13 +7,14 @@
 def call(String namespace, String targetModule) {
 
   dir("${env.WORKSPACE}") {
-    echo "clone tools"
-    echo "install deps"
+    withCredentials([usernamePassword(credentialsId: 'jenkins-folio-rancher', variable: 'KUBECONFIG')]) {
+      echo "$KUBECONFIG"
+      echo "install deps"
 
-    writeFile file: 'requirements.txt', text: libraryResource('org/folio/requirements.txt')
-    sh "pip3 -q install wheel"
-    sh "pip3 install -r requirements.txt"
-    sh "pip3 freeze"
-
+      writeFile file: 'requirements.txt', text: libraryResource('org/folio/requirements.txt')
+      sh "pip3 -q install wheel"
+      sh "pip3 install -r requirements.txt"
+      sh "pip3 freeze"
+    }
   }
 }
