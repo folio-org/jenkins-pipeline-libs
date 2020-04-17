4b7fbf55b1509a949fbbf7badabffb77dbac6c64
Ian Hardy <ian.c.hardy@gmail.com>
2020-04-17 13:12:18 -0400

    towards complete deletion

--- vars/deleteTenant.groovy
+++ vars/deleteTenant.groovy
@@ -1,4 +1,4 @@
-#/usr/bin/env groovy
+#!/usr/bin/env groovy
 
 
 /*
@@ -6,21 +6,38 @@
  */
 
 
-def call(String targetOkapi, String targetTenant) {
+def call(String targetOkapi, String targetTenant, Boolean secured = true) {
 
-  env.okapiToken = ''
+  if (secured) {
+    writeFile file: 'getOkapiToken.sh', text: libraryResource('org/folio/getOkapiToken.sh')
+    sh 'chmod +x getOkapiToken.sh' 
+    env.okapiToken = sh(returnStdout: true, script: "./getOkapiToken.sh -t supertenant -o $targetOkapi -u $user -p $pass").trim()
+  }
+
+  echo "${env.okapiToken}"
 
   httpRequest acceptType: 'APPLICATION_JSON_UTF8',
               contentType: 'APPLICATION_JSON_UTF8',
               consoleLogResponseBody: false,
               httpMode: 'GET',
               validResponseCodes: '200',
-              outputFile:  "tenants.json",
+              outputFile:  "mods-enabled.json",
               customHeaders: [[maskValue: true,name: 'X-Okapi-Token',value: env.okapiToken],
                              [maskValue: false,name: 'X-Okapi-Tenant',value: 'supertenant']],
-              url: "${targetOkapi}/_/proxy/tenants/${targetTenant}"
+              url: "${targetOkapi}/_/proxy/tenants/${targetTenant}/modules"
 
-  sh "cat tenatns.json"
+  sh "cat mods-enabled.json | jq '[.[] + {\"action\" : \"disable\"}]' > disable.json"
+  sh "cat disable.json"
+
+  //httpRequest acceptType: 'APPLICATION_JSON_UTF8',
+  //            contentType: 'APPLICATION_JSON_UTF8',
+  //            consoleLogResponseBody: false,
+  //            customHeaders: [[maskValue: true,name: 'X-Okapi-Token',value: env.okapiToken], 
+  //                            [maskValue: false,name: 'X-Okapi-Tenant',value: 'supertenant']],
+  //            httpMode: 'POST',
+  //            validResponseCodes: '200',
+  //            requestBody: "{ \"urls\" : [ \"${defaultOkapiUrl}\" ]}",
+  //            url: "${previewOkapiUrl}/_/proxy/pull/modules"
 
   return(true)
 
