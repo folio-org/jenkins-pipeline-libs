9c756100350cb65383da3199cfda53e865fde5e3
Ian Hardy <ian.c.hardy@gmail.com>
2020-04-27 14:06:07 -0400

    fix script names

--- vars/previewTenantCleanup.groovy
+++ vars/previewTenantCleanup.groovy
@@ -9,12 +9,12 @@ def call(String scriptArgs) {
   dir("${env.WORKSPACE}") {
       // set up python environment
       writeFile file: 'requirements.txt', text: libraryResource('org/folio/requirements.txt')
-      writeFile file: 'module-cleanup.py', text: libraryResource('org/folio/module-cleanup.py')
+      writeFile file: 'preview-tenant-cleanup.py', text: libraryResource('org/folio/preview-tenant-cleanup.py')
       sh "pip3 -q install wheel"
       sh "pip3 install -r requirements.txt"
 
       // run script
-      withCredentials([usernamePassword(credentialsId: 'okapi-preview-superuser', passwordVariable: 'pass', usernameVariable: 'user',
+      withCredentials([usernamePassword(credentialsId: 'okapi-preview-superuser', passwordVariable: 'pass', usernameVariable: 'user'),
                       [$class: 'AmazonWebServicesCredentialsBinding',
                        accessKeyVariable: 'AWS_ACCESS_KEY_ID',
                        credentialsId: 'jenkins-aws',
