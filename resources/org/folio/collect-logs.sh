0164684fe6e2f1597f0ee1ef5c2e81efdc8bcc07
John Malconian <malc@indexdata.com>
2021-03-15 17:50:38 -0400

    simplify script args

--- resources/org/folio/collect-logs.sh
+++ resources/org/folio/collect-logs.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-while getopts "m:" opt; do
-    case $opt in
-        m) modules+=("$OPTARG");;
-    esac
-done
-shift $((OPTIND -1))
-
 identifier=logs-$(date +%Y-%m-%d-%H%m%S)
 mkdir /tmp/${identifier}
 
-for module in "${modules[@]}"
+for module in "$@"
 do
-  echo "Collect logs for $module"
+  echo "Collecting logs for $module"
   container=$(sudo docker ps -a | grep "${module}:" | awk '{print $1}')
   sudo docker logs $container >& /tmp/${identifier}/${module}.log
 done
@@ -28,4 +21,4 @@ tar cf ${HOME}/${identifier}.tar ${identifier}
 gzip ${HOME}/${identifier}.tar
 rm -rf /tmp/${identifier}
 
-exit 0
+exit $?
