310d1d9a64c2baf069035888a84095e5543546ef
John Malconian <malc@indexdata.com>
2021-03-16 14:35:33 -0400

    'echo' generated tar file

--- resources/org/folio/collect-logs.sh
+++ resources/org/folio/collect-logs.sh
@@ -5,14 +5,12 @@ mkdir /tmp/${identifier}
 
 for module in "$@"
 do
-  echo "Collecting logs for $module"
   container=$(sudo docker ps -a | grep "${module}:" | awk '{print $1}')
   sudo docker logs $container >& /tmp/${identifier}/${module}.log
 done
 
 # include latest okapi log for good measure
 if [ -f /var/log/folio/okapi/okapi.log ]; then 
-  echo "Collect latest Okapi log"
   cp /var/log/folio/okapi/okapi.log /tmp/${identifier}
 fi
 
@@ -21,4 +19,5 @@ tar cf ${HOME}/${identifier}.tar ${identifier}
 gzip ${HOME}/${identifier}.tar
 rm -rf /tmp/${identifier}
 
-exit $?
+echo "${identifier}.tar.gz"
+
