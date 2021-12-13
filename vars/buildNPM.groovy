9f00d3ee3f2af52dc751bcb3dfaf5f331983281e
John Malconian <malc@indexdata.com>
2021-12-13 15:58:04 -0500

    re-enable 'runScripts' for translations updates

--- vars/buildNPM.groovy
+++ vars/buildNPM.groovy
@@ -168,7 +168,7 @@ def call(body) {
 
               // Stage 'Run NPM scripts' - as parallel jobs
               // Hints: https://issues.jenkins-ci.org/browse/JENKINS-38268
-              if ((runScripts.size() >= 1) && !(env.gitCommitter ==~ /^(?i)(FOLIO Translations Bot)/)) { 
+              if ((runScripts.size() >= 1) { 
                 def scriptJobs = [:]
                 runScripts.each { 
                   it.each { 
