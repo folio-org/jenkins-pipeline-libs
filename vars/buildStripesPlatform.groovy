f70a361b16d7fa85b474a3a946451aaa33bbbc8e
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-01-16 22:22:25 +0600

    adjusted according to Zak's suggestions & addition to previous ticket: FOLIO-3768 part2

--- vars/buildStripesPlatform.groovy
+++ vars/buildStripesPlatform.groovy
@@ -9,7 +9,7 @@ def call(String okapiUrl, String tenant, String branch='') {
 
   def foliociLib = new org.folio.foliociCommands()
 
-  if (!(branch =~ /^[rR]\d-\d{4}(-([rR][cC]|hotfix-\d))?$/ || /\bmaster\b/)) {
+  if (!(branch =~ /^[rR]\d-\d{4}(-([rR][cC]|hotfix-\d))?$/) && !(branch =~ /\bmaster\b/)) {
     sh 'rm -f yarn.lock'
   }
 
