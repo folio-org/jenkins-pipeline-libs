07da8c13538b420db40b89457f55734f5782e09e
Eldiiar <Eldiiar_Duishenaliev@epam.com>
2024-07-30 19:08:07 +0600

    Eureka related image into platform-complete kong & keycloak

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -62,6 +62,7 @@ def call(body) {
           sh "docker tag ${env.name}:${env.version} ${env.dockerRepo}/${env.name}:latest"
           sh "docker push ${env.dockerRepo}/${env.name}:${env.version}"
           sh "docker push ${env.dockerRepo}/${env.name}:latest"
+          updateEurekaFile.Info("${env.name}", "${env.version}")
         }
         // publish readme
         echo "Publish Readme Docker Hub"
